21e7f7cdaa3186c16969147542dcde2af985ec58
arthur ming <mingguobin@live.com>
2019-01-18 14:00:06 +0800

    style: 去除空行和整理import语句

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,12 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-
 import { AppComponent } from './app.component';
-
 import { CoreModule } from './core/core.module';
 import { LayoutModule } from './layout/layout.module';
-import { SharedModule } from './shared/shared.module';
 import { RoutesModule } from './routes/routes.module';
+
 
 @NgModule({
   declarations: [
@@ -20,7 +18,6 @@ import { RoutesModule } from './routes/routes.module';
     HttpClientModule,
     CoreModule,
     LayoutModule,
-   
     RoutesModule
   ],
   providers: [],
